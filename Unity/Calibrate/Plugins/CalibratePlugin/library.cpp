5db181d3538a36dff8fb23f91f924d533ebbd27e
zhaolei <zhaolei@zm.com>
2017-04-25 20:37:32 +0800

    add the testcode

--- Unity/Calibrate/Plugins/CalibratePlugin/library.cpp
+++ Unity/Calibrate/Plugins/CalibratePlugin/library.cpp
@@ -4,9 +4,11 @@
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include "library.h"
 
 #include <map>
+#include <opencv2/imgcodecs.hpp>
 
 using namespace cv;
 typedef std::map<int,Mat> ImageMap;
@@ -45,22 +47,45 @@ int  FindChessboardCorners(int imageid,int vertx,int verty,int flags,int corners
 int PLUGIN_API CreateImage(int buf[],int width,int height)
 {
 
-    Mat mat = Mat(height,width,CV_8UC4);
+    Mat mat = Mat(height,width,CV_8UC3);
 
     for (int row = 0; row < height; ++row)
     {
-        Vec4b *rowbuf = mat.ptr<Vec4b>(row);
+        Vec3b *rowbuf = mat.ptr<Vec3b>(row);
         int *srcbuf = buf + width * row;
         for (int col = 0; col <width; ++col)
         {
             unsigned char *pixel = (unsigned char *)(srcbuf + col);
-            Vec4b temp = Vec4b(pixel[0],pixel[1],pixel[2],pixel[4]);
+            Vec3b temp = Vec3b(pixel[0],pixel[1],pixel[2]);
             rowbuf[col] = temp;
         }
 
     }
     int imageid = GenNewImageID();
     m_ImageMap[imageid] = mat;
+	std::vector<Point2f> _corners;
+	int vertx = 9;
+	int verty = 9;
+
+	bool found = findChessboardCorners(mat, Size(vertx, verty), _corners);
+	if (!found)
+	{
+		return -1;
+	}
+	else
+	{
+		Mat matNew = mat.clone();
+		for (int i = 0; i < _corners.size(); ++i)
+		{
+			Point2f posf = _corners[i];
+			Point pos((int)posf.x, (int)posf.y);
+			circle(matNew,pos, 3, Scalar(1.0, 0, 0, 1.0), 1);
+			
+
+		}
+		imwrite("testnew.png", matNew);
+	}
+
     return imageid;
 }
 void hello() {
--- Unity/Calibrate/Plugins/CalibratePlugin/library.h
+++ Unity/Calibrate/Plugins/CalibratePlugin/library.h
@@ -1,7 +1,7 @@
 #ifndef CALIBRATEPLUGIN_LIBRARY_H
 #define CALIBRATEPLUGIN_LIBRARY_H
 
-#ifdef _WIN32
+#if (defined _WIN32) && (defined PLUGIN_API_BUILD)
 #define PLUGIN_API __declspec(dllexport)
 #else
 #define PLUGIN_API
@@ -9,11 +9,11 @@
 
 extern "C"
 {
-/*
+
     int PLUGIN_API FindChessboardCorners(int imageid,int vertx,int verty,int flags,int corners[],int count[],int maxcount);
     int PLUGIN_API CreateImage(int buf[],int width,int height);
     void PLUGIN_API DeleteImage(int imageid);
-  */
+  
     int PLUGIN_API GetOpenCVVersion();
 }
 void hello();
