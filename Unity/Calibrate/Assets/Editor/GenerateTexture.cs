86b29514fe99efd7e7c6c6c2d915194c6062f76f
Balder <void1981@gmail.com>
2017-05-02 23:55:36 +0800

    add the disc

--- Unity/Calibrate/Assets/Editor/GenerateTexture.cs
+++ Unity/Calibrate/Assets/Editor/GenerateTexture.cs
@@ -55,6 +55,133 @@ public class GenerateTexture : MonoBehaviour
 
             
     }
+
+    static Color32 Black = new Color32(0, 0, 0, 0);
+    static Color32 Blue = new Color32(0, 0, 255, 255);
+    static Color32 White = new Color32(255, 255, 255, 255);
+    static Color32 Green = new Color32(0, 255, 0, 255);
+    static Color32 Red = new Color32(255, 0, 0, 255);
+    static Color32 Yellow = new Color32(255, 255, 0, 255);
+    static Color32 Purple = new Color32(255, 0, 255, 255);
+
+
+    [MenuItem("Texture/Disc-Red-Green")]
+    static public void Disc_Red_Green()
+    {
+        
+        GenerateCircusPattern(512, "disc-red-green.png", White, Red, Green, White);
+    }
+
+    [MenuItem("Texture/Disc-Red-Blue")]
+    static public void Disc_Red_Blue()
+    {
+
+        GenerateCircusPattern(512, "disc-red-blue.png", White, Red, Green, White);
+    }
+
+    [MenuItem("Texture/Disc-Red-Purple")]
+    static public void Disc_Red_Purple()
+    {
+
+        GenerateCircusPattern(512, "disc-red-purple.png", White, Red, Green, White);
+    }
+
+    [MenuItem("Texture/Disc-Green-Blue")]
+    static public void Disc_Green_Blue()
+    {
+
+        GenerateCircusPattern(512, "disc-green-blue.png", White, Red, Green, White);
+    }
+
+    [MenuItem("Texture/Disc-Green-Red")]
+    static public void Disc_Green_red()
+    {
+
+        GenerateCircusPattern(512, "disc-green-red.png", White, Red, Green, White);
+    }
+
+    [MenuItem("Texture/Disc-Green-Purple")]
+    static public void Disc_Green_Purple()
+    {
+
+        GenerateCircusPattern(512, "disc-green-purple.png", White, Red, Green, White);
+    }
+
+    [MenuItem("Texture/Disc-Blue-Red")]
+    static public void Disc_Blue_Red()
+    {
+
+        GenerateCircusPattern(512, "disc-blue-red.png", White, Red, Green, White);
+    }
+
+    [MenuItem("Texture/Disc-Blue-Green")]
+    static public void Disc_Blue_Green()
+    {
+
+        GenerateCircusPattern(512, "disc-blue-green.png", White, Red, Green, White);
+    }
+
+    static int Sqr(int val)
+    {
+        return val * val;
+    }
+
+    static void GenerateCircusPattern(int size, string filename, Color32 colorTL, Color32 colorTR, Color32 colorBL, Color32 colorBR)
+    {
+        int width = size;
+        int height = size;
+        // Create the 2d texture with mipmap
+        Texture2D texture = new Texture2D(width, height, TextureFormat.ARGB32, true);
+        // prepare the content buffer
+
+        Color32[] buffer = new Color32[width * height ];
+        Color32 black = new Color32(0, 0, 0, 255);
+        Color32 white = new Color32(255, 255, 255, 255);
+
+
+        int index = 0;
+        int radius = size / 2;
+        int radiusq = radius * radius;
+        int center = size / 2;
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x, ++index)
+            {
+                // out the circle
+                if (radiusq >= (Sqr(x - center) + Sqr(y - center)))
+                {
+                    if (y < center)
+                    {
+                        if (x < center)
+                        {
+                            buffer [index] = colorBL;
+                        } else
+                        {
+                            buffer [index] = colorBR;
+                        }
+                    } else
+                    {
+                        if (x < center)
+                        {
+                            buffer [index] = colorTL;
+                        } else
+                        {
+                            buffer [index] = colorTR;
+                        }
+                    }
+                }
+
+            }
+        }
+        texture.SetPixels32(buffer);
+        texture.Apply();
+        string path = "Assets/Texture/" + filename;
+        string fullpath = Path.Combine(Directory.GetCurrentDirectory(), path);
+        byte[] imgbuf = texture.EncodeToPNG();
+        Debug.Log(fullpath);
+        File.WriteAllBytes(fullpath, imgbuf);
+        Debug.Log("Generate texture " + fullpath + " done!");
+    }
     // Use this for initialization
     void Start()
     {
